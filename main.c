2fdca9432c125382549b42bd40aaccc5430f0f75
Sakura1806 <3503535801@qq.com>
2025-10-06 12:58:19 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,6 @@
-#include<stdio.h>
-int main()
-{
-    int n, sum = 0;
-    printf("请输入一个自然数:");
-    scanf("%d", &n);
-    while (n <= 100)
-    {
-        sum = sum + n;
-        n++;
-    }
-    printf("结果为：%d\n", sum);
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
     return 0;
 }
